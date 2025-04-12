fb3eebf093dedc9dc22a08e8cdf367256b4554ea
TiagoCarneiroDoNascimento <tiago.tcan@gmail.com>
2025-04-12 16:02:25 -0300

    Update
    Dependencias adicionadas:
    
    -MySQL Driver SQL(MySQL JDBC driver)
    -Lombok Developer Tools(Java annotation library which helps to reduce boilerplate code)
    -Spring Data JPA SQL(Persist data in SQL stores with Java Persistence API using Spring Data and Hibernate.)

---
+++ SmartAcademy/src/main/java/com/example/SmartAcademy/SmartAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAcademyApplication.class, args);
+	}
+
+}
---
+++ SmartAcademy/src/test/java/com/example/SmartAcademy/SmartAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
