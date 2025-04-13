b4719378ef8cfb1e7b9b1e4bb113fa3c47fbd890
TiagoCarneiroDoNascimento <tiago.tcan@gmail.com>
2025-04-13 12:31:07 -0300

    Update
    -Cliente
    -Plano

--- SmartAcademy/src/main/java/com/example/SmartAcademy/models/domain/Plano.java
+++ SmartAcademy/src/main/java/com/example/SmartAcademy/models/domain/Plano.java
@@ -1,4 +1,61 @@
 package com.example.SmartAcademy.models.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "plano")
 public class Plano {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "valor")
+    private Double valor;
+
+    public Plano() {
+    }
+
+    public Plano(String nome, String descricao, Double valor) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
 }
